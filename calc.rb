e92ae987481107c3e369878a44f19da2b22aad7d
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-08-09 17:05:48 +0900

    Update calc.rb

--- calc.rb
+++ calc.rb
@@ -1,40 +1,23 @@
 require 'tk'
 
-entry = TkEntry.new.pack('side'=>'top', 'fill' => 'x', 'expand'=>'true')
+e = TkEntry.new { relief 'sunken' }.pack('fill' => 'x')
 
-pad1 = TkFrame.new { pack('side'=>'top', 'fill'=>'both', 'expand'=>'true') }
-pad2 = TkFrame.new { pack('side'=>'top', 'fill'=>'both', 'expand'=>'true') }
-pad3 = TkFrame.new { pack('side'=>'top', 'fill'=>'both', 'expand'=>'true') }
-pad4 = TkFrame.new { pack('side'=>'top', 'fill'=>'both', 'expand'=>'true') }
+r = 0.25
 
-pads = [pad1, pad2, pad3, pad4]
-
-padnum = 0
-for i in ["789/", "456*", "123-", "0+=C"]
-    padnum += 1
-    for j in i.split(//)
-        TkButton.new(pads[padnum]) {
-            text "#{j}"
-            bind('1', proc {entry.value = eval entry.value }) if j == "="
-            bind('1', proc {entry.value = ""}) if j == "C"
-            bind('1', proc {entry.insert('end', "2")}) if j == "2"
-            bind('1', proc {entry.insert('end', "3")}) if j == "3"
-            bind('1', proc {entry.insert('end', "4")}) if j == "4"
-            bind('1', proc {entry.insert('end', "5")}) if j == "5"
-            bind('1', proc {entry.insert('end', "6")}) if j == "6"
-            bind('1', proc {entry.insert('end', "7")}) if j == "7"
-            bind('1', proc {entry.insert('end', "8")}) if j == "8"
-            bind('1', proc {entry.insert('end', "9")}) if j == "9"
-            bind('1', proc {entry.insert('end', "0")}) if j == "0"
-            bind('1', proc {entry.insert('end', "+")}) if j == "+"
-            bind('1', proc {entry.insert('end', "-")}) if j == "-"
-            bind('1', proc {entry.insert('end', "÷")}) if j == "÷"
-            bind('1', proc {entry.insert('end', "×")}) if j == "×"
-            
-            pack('side'=>'left','fill' => 'both','expand'=>'true')
-            font TkFont.new('times 12')
-        }
-    end
-end
+TkFrame.new { |f|
+  "789*456/123-0+=C".split(//).each_with_index do |w, i|
+  TkButton.new(f) {
+    text "#{w}"
+    bind('1', proc { e.value = eval e.value }) if w == "="
+    bind('1', proc { e.value = "" }) if w == "C"
+    command { e.insert('end', w) }  if w != "C" && w != "="
+    place('relx' => (i % 4) * r, 'rely' => i / 4 * r,
+          'relw' => r, 'relh' => r)
+  }
+  i += 1
+  end
+  height 120
+  width 120
+}.pack('fill' => 'x')
 
 Tk.mainloop
